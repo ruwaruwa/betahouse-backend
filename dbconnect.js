fe362909468ab1f95bc348b1d4f7970db54acab7
ruwaruwa <134623822+ruwaruwa@users.noreply.github.com>
2023-07-25 21:20:07 +0300

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const DB_CON=async()=>{
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/Houses");
+        mongoose.connect("mongodb+srv://amiira123:12345@cluster0.xdvvzfc.mongodb.net/Betahouse");
         console.log('DB connected !')
     } catch (error) {
        console.log(error.message) 
